f8a2b50192c08cf9976d5b7996ae9c6a5d6d090e
AkkarawinS <akkarawinRX@gmail.com>
2026-05-07 10:19:29 +0700

    not complete landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import flairBanner from './assets/Flair (2).png';
+import Radio from "./components/Radio";
+import Head from "./components/Head"
+import Banner from "./components/Banner"
+
 
 export default function App() {
   return (
     <>
-      <main style={{ minHeight: "80vh", padding: "20px" }}>
-        <h1>ยินดีต้อนรับสู่ Audtlist 🎵</h1>
-      </main>
-      <Footer />
+      <div className="flair-banner">
+        <img src={flairBanner} alt="Flair banner" />
+        <div className="flair-overlay">
+          <Nav />
+          <Head />
+          <Radio />
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
